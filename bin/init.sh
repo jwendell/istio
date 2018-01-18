d83901cfba05b940cf1094fe225be90071ff9afc
Jonh Wendell <jonh.wendell@redhat.com>
2018-01-18 13:53:58 -0200

    init.sh: Don't save envoy in vendor dir

--- bin/init.sh
+++ bin/init.sh
@@ -15,7 +15,7 @@
 # limitations under the License.
 #
 
-# Init script downloads or updates envoy and the go dependencies. Called from Makefile, which sets
+# Init script downloads or updates envoy. Called from Makefile, which sets
 # the needed environment variables.
 
 ROOT=$(cd $(dirname $0)/..; pwd)
@@ -31,7 +31,7 @@ OUT=${GO_TOP}/out
 
 export GOPATH=${GOPATH:-$GO_TOP}
 # Normally set by Makefile
-export ISTIO_BIN=${ISTIO_BIN:-${GOPATH}/bin}
+export ISTIO_BIN=${ISTIO_BIN:-${GO_TOP}/bin}
 
 $ROOT/bin/verify_go_version.sh
 
@@ -41,35 +41,13 @@ if [ ${ROOT} != "${GO_TOP:-$HOME/go}/src/istio.io/istio" ]; then
        exit 1
 fi
 
-DEP=${DEP:-$(which dep || echo "${ISTIO_BIN}/dep" )}
-
-# Just in case init.sh is called directly, not from Makefile which has a dependency to dep
-if [ ! -f ${DEP} ]; then
-    DEP=${ISTIO_BIN}/dep
-    unset GOOS && go get -u github.com/golang/dep/cmd/dep
-fi
-
-# Download dependencies if needed
-if [ ! -d vendor/github.com ]; then
-    ${DEP} ensure -vendor-only
-    cp Gopkg.lock vendor/Gopkg.lock
-elif [ ! -f vendor/Gopkg.lock ]; then
-    ${DEP} ensure -vendor-only
-    cp Gopkg.lock vendor/Gopkg.lock
-else
-    diff Gopkg.lock vendor/Gopkg.lock > /dev/null || \
-            ( ${DEP} ensure -vendor-only ; \
-              cp Gopkg.lock vendor/Gopkg.lock)
-fi
-
 # Original circleci - replaced with the version in the dockerfile, as we deprecate bazel
 #ISTIO_PROXY_BUCKET=$(sed 's/ = /=/' <<< $( awk '/ISTIO_PROXY_BUCKET =/' WORKSPACE))
 #PROXYVERSION=$(sed 's/[^"]*"\([^"]*\)".*/\1/' <<<  $ISTIO_PROXY_BUCKET)
 PROXYVERSION=$(grep envoy-debug pilot/docker/Dockerfile.proxy_debug  |cut -d: -f2)
 PROXY=debug-$PROXYVERSION
 
-# Save envoy in vendor, which is cached
-if [ ! -f vendor/envoy-$PROXYVERSION ] ; then
+if [ ! -f $ISTIO_BIN/envoy-$PROXYVERSION ] ; then
     mkdir -p $OUT
     pushd $OUT
     # New version of envoy downloaded. Save it to cache, and clean any old version.
@@ -100,18 +78,15 @@ if [ ! -f vendor/envoy-$PROXYVERSION ] ; then
     fi
 
     ${DOWNLOAD_COMMAND} https://storage.googleapis.com/istio-build/proxy/envoy-$PROXY.tar.gz | tar xz
-    cp usr/local/bin/envoy $ISTIO_GO/vendor/envoy-$PROXYVERSION
-    rm -f ${ISTIO_BIN}/envoy ${ROOT}/pilot/proxy/envoy/envoy
+    rm -f ${ISTIO_BIN}/envoy-*
+
+    mkdir -p $ISTIO_BIN
+    cp usr/local/bin/envoy $ISTIO_BIN/envoy-$PROXYVERSION
     popd
-fi
 
-if [ ! -f $GO_TOP/bin/envoy ] ; then
-    mkdir -p $GO_TOP/bin
     # Make sure the envoy binary exists.
-    cp $ISTIO_GO/vendor/envoy-$PROXYVERSION ${ISTIO_BIN}/envoy
-fi
+    ln -sf $ISTIO_BIN/envoy-$PROXYVERSION $ISTIO_BIN/envoy
 
-# Deprecated, may still be used in some tests
-if [ ! -f ${ROOT}/pilot/proxy/envoy/envoy ] ; then
-    ln -sf ${ISTIO_BIN}/envoy ${ROOT}/pilot/proxy/envoy
+    # Deprecated, may still be used in some tests
+    ln -sf $ISTIO_BIN/envoy ${ROOT}/pilot/proxy/envoy
 fi
