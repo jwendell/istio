855d569aee119840182a577e2d344a22663b9512
Jonh Wendell <jonh.wendell@redhat.com>
2019-04-08 19:30:22 -0400

    Update OpenShift dependencies; Drop [deprecated] legacy schema

--- pilot/pkg/kube/inject/initializer.go
+++ pilot/pkg/kube/inject/initializer.go
@@ -52,8 +52,7 @@ var (
 
 		{v1.SchemeGroupVersion, &v1.List{}, "lists", "/apis"},
 
-		{openshiftv1.SchemeGroupVersion, &openshiftv1.DeploymentConfig{}, "deploymentconfigs", "/apis"},
-		{openshiftv1.LegacySchemeGroupVersion, &openshiftv1.DeploymentConfig{}, "deploymentconfigs", "/apis"},
+		{openshiftv1.GroupVersion, &openshiftv1.DeploymentConfig{}, "deploymentconfigs", "/apis"},
 	}
 	injectScheme = runtime.NewScheme()
 )
